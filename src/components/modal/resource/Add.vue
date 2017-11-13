ea127c5b6aaa14a8ccf86efb34012ae67a7b8f6f
Ozzy <heyozramos@gmail.com>
2017-11-12 18:20:44 -0800

    Adds a basic form

--- src/components/modal/resource/Add.vue
+++ src/components/modal/resource/Add.vue
@@ -6,7 +6,21 @@
         button.btn.btn-clear.float-right(@click='closeModal')
         .modal-title.h5 Add Resource
       .modal-body
-        .content.form-group
+        form.content.form-horizontal
+          .form-group
+            .col-3
+              label.form-label(for='modal/resource/Add:title') Title
+            .col-9
+              input.form-input(id='modal/resource/Add:title' type='text' placeholder='Resource Title')
+          .form-group
+            .col-3
+              label.form-label(for='modal/resource/Add:address') Address
+            .col-9
+              input.form-input(id='modal/resource/Add:address' type='text' placeholder='Resource Address')
+          .form-group
+            .col-12
+              label.form-label(for='modal/resource/Add:notes') Notes
+              textarea.form-input(id='modal/resource/Add:notes' placeholder='Resource Notes (location details, hours, restrictions, etc)' rows=5)
       .modal-footer
         button.btn.btn-primary(:class='{loading: isLoading}') Add Resource
 </template>
